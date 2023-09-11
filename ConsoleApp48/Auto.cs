13a36a5552fe43513be66bc34fe288a48ee29c9d
Levente Major <leventemajor36@gmail.com>
2023-09-11 11:21:52 +0200

    Add project files.

---
+++ ConsoleApp48/Auto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onvezeto
+{
+    class Auto
+    {
+        public string azonosito { get; set; }
+        public int teljeseitmeny { get; set; }
+        public double tomeg { get; set; }
+        public int gyorsulas { get; set; }
+        public int beavatkozas { get; set; }
+
+        public Auto(string sor)
+        {
+            var darabok = sor.Split(';');
+            this.azonosito = darabok[0];
+            this.teljeseitmeny = int.Parse(darabok[1]);
+            this.tomeg = double.Parse(darabok[2]);
+            this.gyorsulas = int.Parse(darabok[3]);
+            this.beavatkozas = int.Parse(darabok[4]);
+        }
+    }
+}
---
+++ ConsoleApp48/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Onvezeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Auto> autok = new List<Auto>();
+            StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\autoadat.txt",
+                encoding: Encoding.UTF8);
+
+
+            //2.Feladat
+            Console.WriteLine("2.Feladat: Fajl beolvasva");
+            while (!sr.EndOfStream)
+            {
+                autok.Add(new Auto(sr.ReadLine()));
+            }
+            Console.WriteLine();
+
+
+            //3.Feladat
+            int leggyorsabb = autok.Min(auto => auto.gyorsulas);
+
+            foreach (Auto auto in autok)
+            {
+                if (auto.gyorsulas == leggyorsabb)
+                {
+                    Console.WriteLine($"3.Feladat: {auto.azonosito} {auto.teljeseitmeny} {auto.tomeg} {auto.gyorsulas} {auto.beavatkozas}");
+                }
+                else { }
+            }
+            Console.WriteLine();
+
+
+            //4.Feladat
+            int onallotlan = autok.Max(auto => auto.beavatkozas);
+
+            foreach (Auto auto in autok)
+            {
+                if (auto.beavatkozas == onallotlan)
+                {
+                    Console.WriteLine($"4.Feladat: {auto.azonosito}");
+                }
+            }
+            Console.WriteLine();
+            //int onallo = 0;
+            //foreach (var auto in autok)
+            //{
+            //    if (onallo < auto.beavatkozas)
+            //    {
+            //        onallo = auto.beavatkozas;
+            //    }
+            //}
+            //foreach (Auto auto in autok)
+            //{
+            //    if (auto.beavatkozas == onallo)
+            //    {
+            //        Console.WriteLine($"4.Feladat: {auto.azonosito}");
+            //    }
+            //}
+
+
+            //5.Feladat
+            double atlagtomeg = autok.Average(auto => auto.tomeg);
+            Console.WriteLine("5.Feladat: {0}", atlagtomeg);
+            Console.WriteLine();
+
+
+            //6.Feladat
+            int id = 1;
+
+            using (StreamWriter sw = new StreamWriter(@"..\..\..\src\new_file.txt"))
+            {
+                foreach (Auto auto in autok)
+                {
+                    sw.WriteLine($"{id} {auto.tomeg * 1000}kg");
+                    id++;
+                }
+            }
+            Console.WriteLine("6.Feladat: Fajl kiirva");
+            Console.ReadLine();
+        }
+    }
+}
